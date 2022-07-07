747a0c5dad3223960354b6763d36a211794d9263
Denis Kozarezov <52127090+DenisKozarezov@users.noreply.github.com>
2022-07-07 19:45:37 +0300

    Added GodParametersView

--- Assets/Scripts/Infrastructure/GameSettingsInstaller.cs
+++ Assets/Scripts/Infrastructure/GameSettingsInstaller.cs
@@ -6,6 +6,10 @@ namespace Core.Infrastructure
     [CreateAssetMenu(fileName = "Game Settings", menuName = "Installers/Game Settings")]
     public class GameSettingsInstaller : ScriptableObjectInstaller<GameSettingsInstaller>
     {
+        [Header("Settings")]
+        [SerializeField, Min(0)]
+        private int GameTime;
+
         public override void InstallBindings()
         {
             Container.Bind<ILogger>().FromInstance(new StandaloneLogger()).AsSingle();
